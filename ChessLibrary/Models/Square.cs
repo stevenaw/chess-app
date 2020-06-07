64ff27409b62d2ba8e64d9465baf941e3c096130
stevenaw <sweerdenburg@gmail.com>
2020-06-07 15:05:23 -0400

    Add Equals/Hashcode on some structs

--- ChessLibrary/Models/Square.cs
+++ ChessLibrary/Models/Square.cs
@@ -10,5 +10,30 @@
             File = file;
             Rank = rank;
         }
+
+        public override bool Equals(object obj)
+        {
+            return (obj is Square other) && Equals(other);
+        }
+
+        public bool Equals(Square obj)
+        {
+            return (File == obj.File && Rank == obj.Rank);
+        }
+
+        public override int GetHashCode()
+        {
+            return (File << 16) | (Rank & 16);
+        }
+
+        public static bool operator ==(Square left, Square right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Square left, Square right)
+        {
+            return !(left == right);
+        }
     }
 }
